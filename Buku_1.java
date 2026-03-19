0160fa66c2d520a45b908d73a798b1668bf73146
natanindhita <natalieprasakti@student.ub.ac.id>
2026-03-19 22:40:12 +0700

    Update Buku_1.java

--- Buku_1.java
+++ Buku_1.java
@@ -9,42 +9,6 @@ class Buku {
         this.penulis = penulis;
     }
     
-    int hitungJumlahKata() {
-        String[] kata = sinopsis.split(" ");
-        return kata.length;
-    }
-    
-    int cekKesamaan(Buku bukuLain) {
-        int sama = 0;
-        int total = 3;
-        
-        if (this.judul.equals(bukuLain.judul)) {
-            sama++;
-        }
-        
-        if (this.sinopsis.equals(bukuLain.sinopsis)) {
-            sama++;
-        }
-        
-        if (this.penulis.nama.equals(bukuLain.penulis.nama)) {
-            sama++;
-        }
-        
-        return (sama * 100) / total;
-    }
-    
-    Buku copy() {
-        return new Buku(this.judul, this.sinopsis, this.penulis);
-    }
-    
-    void tampilkanBuku() {
-        System.out.println("judul buku: " + judul);
-        System.out.println("penulis: " + penulis.nama);
-        System.out.println("sinopsis: " + sinopsis);
-        System.out.println("jumlah kata sinopsis: " + hitungJumlahKata());
-        System.out.println();
-    }
-    
     void bacaFile(String pathFile) {
     try {
         java.io.BufferedReader br = new java.io.BufferedReader(new java.io.FileReader(pathFile));
